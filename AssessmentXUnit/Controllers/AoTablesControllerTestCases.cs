68a467e11c39232c1d438fb6525f7be16deea18e
Anumon Jacob <anumon.jacob@nestgroup.net>
2023-10-04 07:33:29 +0530

    TestCases Updation

--- AssessmentXUnit/Controllers/AoTablesControllerTestCases.cs
+++ AssessmentXUnit/Controllers/AoTablesControllerTestCases.cs
@@ -12,6 +12,10 @@ using AngularAssessmentAPI.Controllers;
 using AngularAssessmentAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Azure;
+using Humanizer;
+using Newtonsoft.Json.Linq;
+using static Azure.Core.HttpHeader;
+using Xunit;
 
 namespace AssessmentXUnit.Controllers
 {
@@ -67,12 +71,32 @@ namespace AssessmentXUnit.Controllers
                 Assert.Equal(400, result.StatusCode);
                 Assert.Equal("Data Not Found", result.Value);
             }
+
         [Fact]
-        public async Task UpdateTable_ReturnsOkForValidUpdate()
+        public async Task GetAllTables_ExceptionOccurs_ReturnsBadRequest()
         {
             // Arrange
+            var tableInterfaceMock = new Mock<ITableInterface>();
+            tableInterfaceMock.Setup(repo => repo.GetAllTables()).ThrowsAsync(new Exception("Sample exception"));
+
+            var controller = new AoTablesController(tableInterfaceMock.Object);
+
+            // Act
+            var result = await controller.GetAllTables();
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var errorMessage = Assert.IsType<string>(badRequestResult.Value);
+            Assert.Equal("Sample exception", errorMessage);
+        }
+
+
+        [Fact]
+        public async Task UpdateTable_ReturnsOkForValidUpdate()
+    {
+        // Arrange
             var id = Guid.NewGuid();
-            var aoTable = new AoTable { Id = id, Name = "UpdatedTable" };
+        var aoTable = new AoTable { Id = id, Name = "UpdatedTable" };
             var mockTableInterface = new Mock<ITableInterface>();
             mockTableInterface.Setup(repo => repo.IsExists(id)).ReturnsAsync(true);
             mockTableInterface.Setup(repo => repo.UpdateTable(id, aoTable)).ReturnsAsync(true);
@@ -87,6 +111,25 @@ namespace AssessmentXUnit.Controllers
             var okResult = result as OkObjectResult;
             Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
             okResult.Value.Should().BeEquivalentTo(new { status = "Success" });
+        }
+
+        [Fact]
+        public async Task UpdateTable_IdNotFound_ReturnsBadRequest()
+        {
+            // Arrange
+            var tableInterfaceMock = new Mock<ITableInterface>();
+            var updatedTable = new AoTable { Id = Guid.NewGuid(), Name = "UpdatedTable" };
+            tableInterfaceMock.Setup(repo => repo.IsExists(updatedTable.Id)).ReturnsAsync(false);
+
+            var controller = new AoTablesController(tableInterfaceMock.Object);
+
+            // Act
+            var result = await controller.UpdateTable(updatedTable.Id, updatedTable);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var errorMessage = Assert.IsType<string>(badRequestResult.Value);
+            Assert.Equal("Id not found", errorMessage);
         }
 
         [Fact]
@@ -127,6 +170,26 @@ namespace AssessmentXUnit.Controllers
         }
 
         [Fact]
+        public async Task UpdateTable_ExceptionOccurs_ReturnsBadRequest()
+        {
+            // Arrange
+            var tableInterfaceMock = new Mock<ITableInterface>();
+            var existingTable = new AoTable { Id = Guid.NewGuid(), Name = "Table1" };
+            tableInterfaceMock.Setup(repo => repo.IsExists(It.IsAny<Guid>())).ReturnsAsync(true);
+            tableInterfaceMock.Setup(repo => repo.UpdateTable(It.IsAny<Guid>(), It.IsAny<AoTable>())).ThrowsAsync(new Exception("Sample exception"));
+
+            var controller = new AoTablesController(tableInterfaceMock.Object);
+
+            // Act
+            var result = await controller.UpdateTable(existingTable.Id, existingTable);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var errorMessage = Assert.IsType<string>(badRequestResult.Value);
+            Assert.Equal("Sample exception", errorMessage);
+        }
+
+        [Fact]
         public async Task AddTable_ReturnsOkForValidTable()
         {
             // Arrange
@@ -158,6 +221,25 @@ namespace AssessmentXUnit.Controllers
             // Assert
             Assert.NotNull(result);
             Assert.Equal(400, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddTable_ExceptionOccurs_ReturnsBadRequest()
+        {
+            // Arrange
+            var tableInterfaceMock = new Mock<ITableInterface>();
+            var newTable = new AoTable { Name = "NewTable" };
+            tableInterfaceMock.Setup(repo => repo.AddTable(It.IsAny<AoTable>())).ThrowsAsync(new Exception("Sample exception"));
+
+            var controller = new AoTablesController(tableInterfaceMock.Object);
+
+            // Act
+            var result = await controller.AddTable(newTable);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var errorMessage = Assert.IsType<string>(badRequestResult.Value);
+            Assert.Equal("Sample exception", errorMessage);
         }
 
         [Fact]
@@ -194,6 +276,25 @@ namespace AssessmentXUnit.Controllers
             Assert.NotNull(result);
             Assert.Equal(400, result.StatusCode);
             Assert.Equal("Id not found", result.Value);
+        }
+        [Fact]
+        public async Task DeleteTable_ExceptionOccurs_ReturnsBadRequest()
+        {
+            // Arrange
+            var tableInterfaceMock = new Mock<ITableInterface>();
+            var tableId = Guid.NewGuid();
+            tableInterfaceMock.Setup(repo => repo.IsExists(It.IsAny<Guid>())).ReturnsAsync(true);
+            tableInterfaceMock.Setup(repo => repo.DeleteTable(It.IsAny<Guid>())).ThrowsAsync(new Exception("Sample exception"));
+
+            var controller = new AoTablesController(tableInterfaceMock.Object);
+
+            // Act
+            var result = await controller.DeleteTable(tableId);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            var errorMessage = Assert.IsType<string>(badRequestResult.Value);
+            Assert.Equal("Sample exception", errorMessage);
         }
     }
 
